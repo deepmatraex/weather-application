1bb64f659fc49ecf8680a5a7a998b37a309b6f1e
Deepak <yadav.deepak2453@gmail.com>
2019-11-27 17:51:10 +0530

    first-project

---
+++ src/app/apixu.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+
+  constructor(private http : HttpClient) {}
+   getWeather(location){
+    return this.http.get(
+      'http://api.weatherstack.com/current?access_key=d5e27cb698cee291ba70471febe49b76&query=New%20York' +location);
+    }
+}
