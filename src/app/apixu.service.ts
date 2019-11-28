38641c9fa78a4b39438e5365cdc2fffb8b58bac8
deepmatraex <58257385+deepmatraex@users.noreply.github.com>
2019-11-28 19:01:43 +0530

    asda
    ad

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,6 +9,6 @@ export class ApixuService {
   constructor(private http : HttpClient) {}
    getWeather(location){
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=d5e27cb698cee291ba70471febe49b76&query=New%20York';
+     'http://api.weatherstack.com/current?access_key=d5e27cb698cee291ba70471febe49b76&query=New%20York' +location);
     }
 }
